37fad84671cabf0e7799448736a0c3b90e760713
Rookit15 <attehiltunen@hotmail.com>
2022-10-24 21:55:03 +0300

    Create First_steps.js
    First steps to going trough codecademy js course

---
+++ First_steps.js
@@ -0,0 +1,9 @@
+//This is command and next command prints to console
+console.log(5)/* This is also comment*/ 
+console.log('This is a string')
+console.log('Hello '+ 'World')
+console.log('This is some sentence'.length)// Prints how many characters are in string before .lenght
+console.log('Hello'.startsWith('H')) //Prints true so .startsWith('') gives boolean
+console.log('                   Very much white space     '.trim()) // trim get's rid of whitespaces
+console.log(Math.random() *100) // return random number from 0 to 100 multiplier tells you the roof
+console.log(Number.isInteger(500)) // checks is the number given is integer
