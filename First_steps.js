8049f559e32580fd2980eed34fff10c048ab23b9
Rookit15 <attehiltunen@hotmail.com>
2022-10-25 18:38:15 +0300

    Weather_app
    first steps finished and Weather_app done where you can print to console Kelvins to different scales

--- First_steps.js
+++ First_steps.js
@@ -7,3 +7,7 @@ console.log('Hello'.startsWith('H')) //Prints true so .startsWith('') gives bool
 console.log('                   Very much white space     '.trim()) // trim get's rid of whitespaces
 console.log(Math.random() *100) // return random number from 0 to 100 multiplier tells you the roof
 console.log(Number.isInteger(500)) // checks is the number given is integer
+var item = 'phone'
+const phoneNumber= 5503621522 // variables are made. Also noticed number does some wired things when it is starting with 0
+let nameOfPerson = 'jack' // name variable can't be set because it is deprecated
+console.log(nameOfPerson , item, phoneNumber)
---
+++ Weather_app.js
@@ -0,0 +1,12 @@
+function Kelvin(){
+    // Kelvin is set to const and it will not change during program
+    const kelvin = 0;
+    // celsius is kelvin degrees but minus 273
+    let celsius = kelvin - 273;
+    let fahrenheit = Math.floor((celsius * (9/5) + 32));
+    let Newton = Math.floor(celsius* (33/100));
+    console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+    console.log(`The temperature is ${Newton} degrees Newton.`)
+    return null
+}
+Kelvin();
